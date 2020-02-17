111b97c7c54fe98e541f99cf83ebf7ecc373da7a
Kurt Kaiser <kurtbkaiser@gmail.com>
2020-02-17 18:55:38 -0500

    Add files via upload

---
+++ EnemyScript.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float moveSpeed = -5;
+    public int score = 0;
+    public Text scoreText;
+
+    private Rigidbody2D myRigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D coll)
+    {
+        if(coll.gameObject.name == "Player")
+        {
+            Respawn();
+            score = score - 1;
+            scoreText.text = score.ToString();
+            if (score < 0)
+            {
+                scoreText.text = "GAME OVER :( !!!!!!";
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.velocity = new Vector2(moveSpeed, myRigidbody.velocity.y);
+        if (transform.position.x < -10)
+        {
+            Respawn();
+            score = score + 1;
+            scoreText.text = score.ToString();
+        }
+    }
+
+    void Respawn()
+    {
+        transform.position = new Vector2(10.0f, -1);
+        transform.localScale += new Vector3(0, Random.Range(-0.1f, 0.1f), 0);
+        moveSpeed = Random.Range(-8.0f, -3.0f);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D myRigidbody;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if(transform.position.y < -0.7f)
+            {
+                myRigidbody.velocity = new Vector2(myRigidbody.velocity.x, jumpForce);
+            }
+        }
+    }
+}
